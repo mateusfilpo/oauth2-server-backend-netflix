a31c32bd3e2a58f3c25919b6447397a6d29b76d4
Mateus Filpo <mateusfilpo27@gmail.com>
2024-11-23 16:16:16 -0300

    Criada estrutura inicial e configuração para o servidor de autenticação do projeto backend netflix

---
+++ src/main/java/br/com/mateusfilpo/netflixauthserver/NetflixAuthServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.mateusfilpo.netflixauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mateusfilpo/netflixauthserver/NetflixAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mateusfilpo.netflixauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
